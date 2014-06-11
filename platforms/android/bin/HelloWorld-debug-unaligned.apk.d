6d29e855621459c4be45ec4fe439394d2bad5561
你的姓名 <>
2014-06-11 21:02:11 +0800

    “提交注释”

---
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\storage\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
+C:\storage\platforms\android\bin\HelloWorld.ap_ \
+C:\storage\platforms\android\bin\classes.dex \
+C:\storage\platforms\android\CordovaLib\bin\classes.jar \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\storage\platforms\android\bin\classes.dex : \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\BuildConfig.class \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\HelloWorld.class \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\R$attr.class \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\R$drawable.class \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\R$string.class \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\R$xml.class \
+C:\storage\platforms\android\bin\classes\com\vmitime\storage\R.class \
+C:\storage\platforms\android\bin\dexedLibs\classes-24c07f30cb84ea542d1a0e0a3c4aac4b.jar \
